01a74da45eee4fb152c7422db4f699aa5fb3ae6f
Kian Javaheri <kianjavaheri911@gmail.com>
2022-05-06 12:44:33 -0700

    5/6/22 - 12:44 PM
    Initial

---
+++ src/Components/Whiteboard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Whiteboard() {
+  return (
+    <div>
+        <nav>
+            <h1>whiteboard</h1>
+        </nav>
+    </div>
+  )
+}
+
+export default Whiteboard
